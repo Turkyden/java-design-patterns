5b534e3a26c67ae826fa0edbeb2f37be0e811ba5
Dengju Deng <wj871287@gmail.com>
2019-05-14 05:23:28 +0000

    Test gitpod

--- proxy/src/main/java/com/iluwatar/proxy/App.java
+++ proxy/src/main/java/com/iluwatar/proxy/App.java
@@ -23,7 +23,7 @@
 package com.iluwatar.proxy;
 
 /**
- * 
+ *
  * A proxy, in its most general form, is a class functioning as an interface to something else. The
  * proxy could interface to anything: a network connection, a large object in memory, a file, or
  * some other resource that is expensive or impossible to duplicate. In short, a proxy is a wrapper
@@ -36,7 +36,7 @@ package com.iluwatar.proxy;
  * <p>
  * In this example the proxy ({@link WizardTowerProxy}) controls access to the actual object (
  * {@link IvoryTower}).
- * 
+ *
  */
 public class App {
 
@@ -51,6 +51,7 @@ public class App {
     proxy.enter(new Wizard("Black wizard"));
     proxy.enter(new Wizard("Green wizard"));
     proxy.enter(new Wizard("Brown wizard"));
+    proxy.enter(new Wizard("Deng Ju"));
 
   }
 }
